a800cab28d20ea1b1483245cf70997556a50ea43
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-11-18 17:26:29 -0800

    sanitize

--- public/main/CandidateItem.js
+++ public/main/CandidateItem.js
@@ -1,15 +1,24 @@
 import Component from '../Component.js';
 
 class CandidateItem extends Component {
-
     renderHTML() {
+        
         const candidate = this.props;
         const name = candidate.candidate_name;
+        const capitalize = (string) => {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        };
+        const sanitizeName = (name) => {
+            let splitName = name.split(', ');
+            const firstName = capitalize(splitName[1].toLowerCase());
+            const lastName = capitalize(splitName[0].toLowerCase());
+            return `${firstName + ' ' + lastName}`;
+        };
         //const id = candidate.candidate_id;
         return /*html*/`
         <div>
             <li>
-                <h2>${name}</h2>
+                <h2>${sanitizeName(name)}</h2>
             </li>
         </div>
         `;
