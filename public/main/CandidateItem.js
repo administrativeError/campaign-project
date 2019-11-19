7548f6d652191db6427631bd5704e58e277ed584
samSterns <sam.g.sterns@gmail.com>
2019-11-19 13:50:29 -0800

    got the money pretified

--- public/main/CandidateItem.js
+++ public/main/CandidateItem.js
@@ -4,6 +4,7 @@ class CandidateItem extends Component {
     renderHTML() {
         
         const candidate = this.props;
+        console.log(candidate);
         const name = candidate.candidate_name;
         const capitalize = (string) => {
             return string.charAt(0).toUpperCase() + string.slice(1);
@@ -15,9 +16,15 @@ class CandidateItem extends Component {
             return `${firstName + ' ' + lastName}`;
         };
         //const id = candidate.candidate_id;
+
+        function numberWithCommas(x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        } 
+
         return /*html*/`
         <li>
             <h2>${sanitizeName(name)}</h2>
+            <h3>$${numberWithCommas(Math.ceil(candidate.cash_on_hand_end_period))}</h3>
         </li>
         `;
     }
