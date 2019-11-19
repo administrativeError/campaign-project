37a8e6fcf6a63f1cd3d3a3ab2e21a730a9a3bbd8
AaronMullan <aaroncmullan@gmail.com>
2019-11-19 09:38:52 -0800

    chart sized better

--- public/compare/graph.js
+++ public/compare/graph.js
@@ -1,39 +1,23 @@
-import staticdata from '/staticdata.js';
+import staticdata from './staticdata.js';
 
-
-staticdata.forEach(function(element) {
-    if(candidate_id.value === 'P00009621') {
-    candidate.name = 'Elizabeth Warren'
-    } 
-});
-console.log(staticdata);
-
-const data = [
-    {from: "> $2000",  to: "Wernie Wanders",  weight:  2230000},
-    {from: "> $1000",  to: "Wernie Wanders", weight:  1990000},
-    {from: "> $500",  to: "Wernie Wanders",   weight:  1180000},
-    {from: "> $200",  to: "Wernie Wanders",   weight:   990000},
-    {from: "< $200",     to: "Wernie Wanders",  weight:   950000},
-
-    {from: "> $2000",  to: "Bamala Berris",  weight:  1230000},
-    {from: "> $1000",  to: "Bamala Berris", weight:  2290000},
-    {from: "> $500",  to: "Bamala Berris",   weight:  1380000},
-    {from: "> $200",  to: "Bamala Berris",   weight:   940000},
-    {from: "< $200",     to: "Bamala Berris",  weight:   1950000},
-    
-   
-  ];
- 
-
+const mungedDataArray = [];
+staticdata.results.forEach(result => {
+  result.from = result.size;
+  result.to = result.candidate_id;
+  result.weight = result.total;
+  mungedDataArray.push(result);
+})
+console.log(mungedDataArray);
   
   // create a chart and set the data
-  const chart = anychart.sankey(data);
+  const chart = anychart.sankey(mungedDataArray);
   
   // set the width of nodes
   chart.nodeWidth("30%");
   
   // set the container id
-  chart.container("container");
+  chart.container("chart");
+  // chart.setSize('100%', '100%');
   
   // initiate drawing the chart
   chart.draw();
