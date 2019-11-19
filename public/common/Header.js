04f61ba5820f56c08e32f70dbd00d9710ad3817a
samSterns <sam.g.sterns@gmail.com>
2019-11-18 17:51:08 -0800

    slay the day away and fixed log out and header

--- public/common/Header.js
+++ public/common/Header.js
@@ -1,21 +1,27 @@
 import Component from '../Component.js';
 
 class Header extends Component {
-    // onRender(header) {
-    //     const logoutLink = header.querySelector('.logout');
-    //     logoutLink.addEventListener('click', () => {
-
-    //     });
-    // }
+    onRender(header) {
+        if (!localStorage.getItem('USER')) {
+            const nav = header.querySelector('.main-nav');
+            nav.style.display = 'none';
+        }
+        const logoutLink = header.querySelector('.logout');
+        logoutLink.addEventListener('click', () => {
+            if (localStorage.getItem('USER')) {
+                localStorage.removeItem('USER');
+            } 
+        });
+    }
     renderHTML() {
         return /*html*/ `
     <header>
         <h1>Administrative Error</h1> 
-        <p>
+        <nav class="main-nav">
             <a href="../main/main.html">Candidates</a>
             <a href="../compare/compare-graph.html">Compare</a>
-            <a class="logout" href="../login-page/login.html">Logout</a>
-        </p>
+            <a class="logout" href="../index.html">Logout</a>
+        </nav>
     </header>
         `;
     }
