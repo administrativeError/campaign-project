1a624f06b94ec8eae78bfd5ebda47b9d1aca1967
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-11-20 11:15:23 -0800

    selections availible hopefully

--- public/compare/CompareGraphApp.js
+++ public/compare/CompareGraphApp.js
@@ -1,26 +1,24 @@
 import Componenet from '../Component.js';
 import Header from '../common/Header.js';
 import { loadGraph } from './graph.js';
-import { getFavorites, getCandidates } from '../services/api.js';
+import { getFavorites, getCandidates, addAFavorite, deleteAFavorite } from '../services/api.js';
 
 
 class GraphApp extends Componenet {
 
-    onRender(dom){
+    async onRender(dom){
         const header = new Header();
         const headerDOM = header.renderDOM();
         dom.prepend(headerDOM);
         const graphSection = dom.querySelector('.graph');
         const graph = loadGraph();
         graphSection.appendChild(graph);
-        const favoritesIds = getFavorites();
-        // let i = 0;
+        const favoritesIds = await getFavorites();
         const favoritesIdsObject = favoritesIds.reduce((acc, curr) => {
             acc[curr] = curr; 
-            // i ++;
             return acc;
         }, {});
-        const candidates = getCandidates();
+        const candidates = await getCandidates();
         const candidateNamesAndIds = candidates.results.map(candidate =>{
             return { name : candidate.candidate_name, id: candidate.candidate_id };
         });
@@ -30,9 +28,38 @@ class GraphApp extends Componenet {
         const addCandidateSelect = document.getElementById('add');
         candidatesNotFavorites.forEach(candidate => {
             const option = document.createElement('option');
-            option.textContent(candidate.name);
+            option.textContent = candidate.name;
+            option.value = candidate.id;
             addCandidateSelect.appendChild(option);
         });
+        const candidatesAreFavorites = candidateNamesAndIds.filter(candidate => {
+            return candidate.id !== favoritesIdsObject[candidate.id];
+        });
+        const removeCandidateSelect = document.getElementById('remove');
+        candidatesAreFavorites.forEach(candidate => {
+            const option = document.createElement('option');
+            option.textContent = candidate.name;
+            option.value = candidate.id;
+            removeCandidateSelect.appendChild(option);
+        });
+
+        addCandidateSelect.addEventListener('change', async(event) => {
+            const id = event.target.value;
+            const thisFavorite = {
+                candidate_id: id
+            };
+            await addAFavorite(thisFavorite);
+            loadGraph();
+        });
+        removeCandidateSelect.addEventListener('change', async(event) => {
+            const id = event.target.value;
+            const thisFavorite = {
+                candidate_id: id
+            };
+            await deleteAFavorite(thisFavorite);
+            loadGraph();
+        });
+
     }
 
     renderHTML(){
