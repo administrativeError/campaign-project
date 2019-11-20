8f1f16fde221ee6d1b9d08fea128b74643497fef
Matt Munch <mattmunch94@gmail.com>
2019-11-20 11:53:21 -0800

    favorites rendering

--- public/compare/graph.js
+++ public/compare/graph.js
@@ -1,40 +1,53 @@
 import { getCandidateCashData, getCandidates } from '../services/api.js';
-const loadGraph = async () => {
-
-
-const realData = await getCandidateCashData();
-const realCandidates = await getCandidates();
-console.log(realCandidates);
-
-const mungedDataArray = realData.results.map(result => {
-  result.from = '> $' + result.size;
-  result.id = result.candidate_id;
-  result.weight = result.total;
-return result
-})
-
-console.log(mungedDataArray);
-
-realCandidates.results.forEach(candidate => {
-      const matches = mungedDataArray.filter(match => candidate.candidate_id === match.id);
-      matches.forEach(obj => obj.to = candidate.candidate_name)
-});
-
-  // create a chart and set the data
-const chart = anychart.sankey(mungedDataArray);
+import { getFavorites } from '../services/api.js';
+const loadGraph = async() => {
+    const favorites = await getFavorites();
   
+    console.log(favorites);
+  
+    
+    const realData = await getCandidateCashData();
+    const realCandidates = await getCandidates();
+    const mungedDataArray = realData.results.map(result => {
+        result.from = '> $' + result.size;
+        result.id = result.candidate_id;
+        result.weight = result.total;
+        return result;
+    });
+    
+    realCandidates.results.forEach(candidate => {
+        const matches = mungedDataArray.filter(match => candidate.candidate_id === match.id);
+        matches.forEach(obj => obj.to = candidate.candidate_name);
+    });
+    let displayData = [];
+    let favoriteData = [];
+    if (favorites !== []) {
+        favorites.forEach(fav => {
+            favoriteData.push(mungedDataArray.filter(data => fav.candidate_id === data.candidate_id));
+        });
+        displayData = favoriteData.flat();
+        
+    } else console.log(mungedDataArray);
+    mungedDataArray.forEach(item => {
+        displayData.push(item) ;
+    });
+
+    
+    console.log(displayData);
+    // create a chart and set the data
+    const chart = anychart.sankey(displayData);
   // set the width of nodes
-chart.nodeWidth('30%');
+    chart.nodeWidth('30%');
   
   // set the container id
-chart.container('chart');
+    chart.container('chart');
   // chart.setSize('100%', '100%');
   
-  const title = chart.title();
-  title.text('Candidate Donations by Dollar Amount');
-  title.enabled(true);
+    const title = chart.title();
+    title.text('Candidate Donations by Dollar Amount');
+    title.enabled(true);
 
   // initiate drawing the chart
-chart.draw();
+    chart.draw();
 };
 loadGraph();
