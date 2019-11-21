52332a24bebe587bba1212feeccfa14120ee939b
Matt Munch <mattmunch94@gmail.com>
2019-11-20 17:39:07 -0800

    fixed bug where user can return to login page while logged in

--- public/login-page/login-index.js
+++ public/login-page/login-index.js
@@ -1,4 +1,7 @@
 import AuthApp from './LoginApp.js';
-
+if (localStorage.getItem('USER')) {
+    window.location = '../main/main.html';
+    alert('Must log out to return to log in page.');
+}
 const app = new AuthApp();
 document.body.prepend(app.renderDOM());
