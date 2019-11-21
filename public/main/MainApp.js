ac27ba274107ec43eb2200d7ca0895aef409628d
Matt Munch <mattmunch94@gmail.com>
2019-11-21 14:29:22 -0800

    cleaned up some whitespace and got year rendering dynamically in the chart title

--- public/main/MainApp.js
+++ public/main/MainApp.js
@@ -1,28 +1,23 @@
 import Component from '../Component.js';
 import CandidateList from './CandidateList.js';
-import CompareButton from '../common/CompareButton.js'
+import CompareButton from '../common/CompareButton.js';
 import Header from '../common/Header.js';
-import Footer from '../common/Footer.js';
 import { getCandidates } from '../services/api.js';
 import Loading from '../common/Loading.js';
 
 class MainApp extends Component {
     async onRender(dom) {
-
         const main = dom.querySelector('main');
-
         const header = new Header();
         dom.prepend(header.renderDOM());
 
         const loading = new Loading();
-        console.log(loading.renderDOM())
         dom.appendChild(loading.renderDOM());
         localStorage.setItem('YEAR', '2020');
         const yearArray = [];
         for (let i = 2020; i > 1979; i = i - 4){
             yearArray.push(i);
         }
-        console.log(yearArray);
         const yearSelect = dom.querySelector('.select-year');
         yearArray.forEach(year => {
             const option = document.createElement('option');
@@ -30,20 +25,11 @@ class MainApp extends Component {
             option.value = year;
             yearSelect.appendChild(option);
         });
-        console.log(yearSelect.value);
         
         const candidates = await getCandidates(yearSelect.value);
         
         const candidateList = new CandidateList({ candidates });
         main.appendChild(candidateList.renderDOM());
-        
-        // const compareButtonListener = dom.querySelectorAll('#compare-button-bottom');
-        
-   
-        // compareButtonListener.addEventListener('click', () => {
-        //     window.location='../compare/compare-graph.html';
-        // });
-
         yearSelect.addEventListener('change', async(event) => {
 
             const value = event.target.value;
@@ -51,23 +37,9 @@ class MainApp extends Component {
                 localStorage.removeItem('YEAR');
                 localStorage.setItem('YEAR', value);
             } else localStorage.setItem('YEAR', value);
-            console.log(value);
             const candidates = await getCandidates(value);
-            console.log(candidates);
             candidateList.update({ candidates });
         });
-        try {
-            // const candidates = await getTopTwentyCandidates();
-            // candidateList.update({ candidates });
-        }
-        catch (err) {
-            console.log('Load candidates failed\n', err);
-        }
-        finally {
-            setTimeout(() => {
-                loading.update({ loading: false });
-            }, 500);
-        }
         const compareButton = new CompareButton();
         main.appendChild(compareButton.renderDOM());
         
